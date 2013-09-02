c6907c7b1719bbc8b2356eb973ed25ddb28a53a8
bdl <bodydomelight@gmail.com>
2013-09-02 09:34:22 +0300

    Fibonacci number through recursion.

---
+++ src/test/Fib.java
@@ -0,0 +1,34 @@
+package test;
+
+public class Fib {
+
+    public static long fib(int n) {
+        if (n < 0) {
+            throw new ArithmeticException();
+        }
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n - 2) + fib(n - 1);
+    }
+
+    public static void main(String[] args) {
+        long[] testData = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,
+            377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711,
+            28657, 46368, 75025, 121393, 196418, 317811, 514229,
+            832040, 1346269, 2178309, 3524578, 5702887, 9227465,
+            14930352, 24157817, 39088169};
+        for (int i = 0; i < 30; i++) {
+            long f = fib(i);
+            if (f == testData[i]) {
+                System.out.print(fib(i) + "; ");
+            } else {
+                System.out.println(f + "doesn't equal" + testData[i]);
+            }
+        }
+
+    }
+}
