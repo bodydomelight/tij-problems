ebcaadf748a381ffea2da253fb8379a0c98e4f70
bdl <bodydomelight@gmail.com>
2013-09-11 15:45:48 +0300

    p36

--- src/test/RandomTest.java
+++ src/test/RandomTest.java
@@ -3,11 +3,13 @@ package test;
 import java.util.Random;
 
 public class RandomTest {
+
     static Random rand = new Random();
+
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        for(int i = 0; i < 1600000; i++){
-            System.out.println(rand.nextFloat());
+        for (int i = 0; i < 1600000; i++) {
+            rand.nextFloat();
         }
         long end = System.currentTimeMillis();
         System.out.println("Time taken: " + (end - start) + " ms");
