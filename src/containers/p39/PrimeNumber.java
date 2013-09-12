d48cba8842fcae2b529438979a855f965a44aeb5
bdl <bodydomelight@gmail.com>
2013-09-12 23:40:50 +0300

    optimized p39

--- src/containers/p39/PrimeNumber.java
+++ src/containers/p39/PrimeNumber.java
@@ -12,21 +12,23 @@ public class PrimeNumber {
             return Integer.MAX_VALUE;
         }
         int size;
-        if (number > (int) (Integer.MAX_VALUE * 0.9)) {
+        if (number > (int) Integer.MAX_VALUE * 0.99999) {
             size = Integer.MAX_VALUE - 1;
         } else {
-            size = (int) (number * 1.1) + 1;
+            size = (int) (number * 1.000005) + 1;
         }
-        boolean[] ints = new boolean[size + 1];
+        boolean[] ints = new boolean[size + 1 - number];
         int factor = (int) Math.floor(Math.sqrt(size));
         for (int i = 2; i <= factor; i++) {
             for (int j = i * 2; j <= size; j += i) {
-                ints[j] = true;
+                if (j - number > 0) {
+                    ints[j - number] = true;
+                }
             }
         }
-        for (int i = number + 1; i < ints.length; i++) {
+        for (int i = 1; i < ints.length; i++) {
             if (!ints[i]) {
-                return i;
+                return i + number;
             }
         }
         return getNextPrimeNumberAfter(size);
@@ -37,8 +39,7 @@ public class PrimeNumber {
             System.out.println("Next prime number after " + i + " is " + getNextPrimeNumberAfter(i));
         }
         System.out.println();
-
-        int n = 1000000;
+        int n = 1000000000;
         long start = System.currentTimeMillis();
         System.out.println("Next prime number after " + n + " is " + getNextPrimeNumberAfter(n));
         long total = System.currentTimeMillis() - start;
