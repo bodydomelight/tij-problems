61576453cf91c11434f2320927000418b9ac5e1e
bdl <bodydomelight@gmail.com>
2014-01-09 01:28:37 +0200

    enums.po3-7 solved

--- src/enums/p07/EnumSetTest.java
+++ src/enums/p07/EnumSetTest.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package enums.p07;
 
 import java.util.EnumSet;
 
-/**
- *
- * @author rage
- */
 public class EnumSetTest {
     enum Test {
         A, B, C
